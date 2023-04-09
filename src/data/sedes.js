0c3443b1a06d53c3a6bab255aae91d33431ad962
Oscar Ruiz <oscarruiz2614@gmail.com>
2023-04-09 13:41:16 -0500

    fix

---
+++ src/data/sedes.js
@@ -0,0 +1,24 @@
+const sedes = [
+    {
+        name: 'Oficina Principal',
+        address: 'Av. 26 No. 4-82, Neiva - Huila',
+        cant: 4
+    },
+    {
+        name: 'Agencia Pitalito',
+        address: 'Terminal Pitalito',
+        cant: 2
+    },
+    {
+        name: 'Agencia Bogotá Salitre',
+        address: 'Terminal Bogotá Salitre',
+        cant: 2
+    },
+    {
+        name: 'Agencia Garzón',
+        address: 'Terminal Garzón',
+        cant: 2
+    },
+];
+
+export default sedes;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
     <div>
 		<!-- banner -->
 		<Banner/>
+
         <section class="categories-section pt-100 pb-70">
 			<div class="container">
 				<div class="section-title text-center">
@@ -93,319 +94,93 @@
 			</div>
 		</section>
 		<!-- Category Section End -->
-		<!-- Jobs Section Start -->
-		<!-- <section class="job-section pb-70">
+		
+		<!-- Way To Use Section Start -->
+		<section class="use-section pt-100 pb-70">
 			<div class="container">
 				<div class="section-title text-center">
-					<h2>Jobs You May Be Interested In</h2>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices.</p>
+					<h2>Postulate</h2>
 				</div>
 
 				<div class="row">
-					<div class="col-sm-6">
-						<div class="job-card">
-							<div class="row align-items-center">
-								<div class="col-lg-3">
-									<div class="thumb-img">
-										<a href="job-details.html">
-											<img src="assets/img/company-logo/1.png" alt="company logo">
-										</a>
-									</div>
-								</div>
-
-								<div class="col-lg-6">
-									<div class="job-info">
-										<h3>
-											<a href="job-details.html">Post-Room Operate</a>
-										</h3>
-										<ul>
-											<li>Via <a href="#">Tourt Design LTD</a></li>
-											<li>
-												<i class='bx bx-location-plus'></i>
-												Wellesley Rd, London
-											</li>
-											<li>
-												<i class='bx bx-filter-alt' ></i>
-												Accountancy
-											</li>
-											<li>
-												<i class='bx bx-briefcase' ></i>
-												Freelance
-											</li>
-										</ul>
-									</div>
-								</div>
-
-								<div class="col-lg-3">
-									<div class="job-save">
-										<span>Full Time</span>
-										<a href="#">
-											<i class='bx bx-heart'></i>
-										</a>
-										<p>
-											<i class='bx bx-stopwatch' ></i>
-											1 Hr Ago
-										</p>
-									</div>
-								</div>
-							</div>
+					<div class="col-md-4 col-sm-6">
+						<div class="use-text">
+							<span>1</span>
+							<i class='flaticon-website'></i>
+							<h3>Busca nuestras vacantes</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </p>
 						</div>
 					</div>
 
-					<div class="col-sm-6">
-						<div class="job-card">
-							<div class="row align-items-center">
-								<div class="col-lg-3">
-									<div class="thumb-img">
-										<a href="job-details.html">
-											<img src="assets/img/company-logo/2.png" alt="company logo">
-										</a>
-									</div>
-								</div>
-
-								<div class="col-lg-6">
-									<div class="job-info">
-										<h3>
-											<a href="job-details.html">Data Entry</a>
-										</h3>
-										<ul>
-											<li>Via <a href="#">Techno Inc.</a></li>
-											<li>
-												<i class='bx bx-location-plus'></i>
-												Street 40/A, London
-											</li>
-											<li>
-												<i class='bx bx-filter-alt' ></i>
-												Data Entry
-											</li>
-											<li>
-												<i class='bx bx-briefcase' ></i>
-												Freelance
-											</li>
-										</ul>
-									</div>
-								</div>
-
-								<div class="col-lg-3">
-									<div class="job-save">
-										<a href="#">
-											<i class='bx bx-heart'></i>
-										</a>
-										<p>
-											<i class='bx bx-stopwatch' ></i>
-											3 Hr Ago
-										</p>
-									</div>
-								</div>
-							</div>
+					<div class="col-md-4 col-sm-6">
+						<div class="use-text">
+							<span>2</span>
+							<i class='flaticon-recruitment'></i>
+							<h3>Postulate a la que mas te guste</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </p>
 						</div>
 					</div>
 
-					<div class="col-sm-6">
-						<div class="job-card">
-							<div class="row align-items-center">
-								<div class="col-lg-3">
-									<div class="thumb-img">
-										<a href="job-details.html">
-											<img src="assets/img/company-logo/3.png" alt="company logo">
-										</a>
-									</div>
-								</div>
-
-								<div class="col-lg-6">
-									<div class="job-info">
-										<h3>
-											<a href="job-details.html">Graphic Designer</a>
-										</h3>
-										<ul>
-											<li>Via <a href="#">Devon Design</a></li>
-											<li>
-												<i class='bx bx-location-plus'></i>
-												West Sight, USA
-											</li>
-											<li>
-												<i class='bx bx-filter-alt' ></i>
-												Graphics
-											</li>
-											<li>
-												<i class='bx bx-briefcase' ></i>
-												Freelance
-											</li>
-										</ul>
-									</div>
-								</div>
-
-								<div class="col-lg-3">
-									<div class="job-save">
-										<a href="#">
-											<i class='bx bx-heart'></i>
-										</a>
-										<p>
-											<i class='bx bx-stopwatch' ></i>
-											4 Hr Ago
-										</p>
-									</div>
-								</div>
-							</div>
+					<div class="col-md-4 col-sm-6 offset-sm-3 offset-md-0">
+						<div class="use-text">
+							<span>3</span>
+							<i class='flaticon-login'></i>
+							<h3>Envia tu hoja de vida</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </p>
 						</div>
 					</div>
+				</div>
+			</div>
+		</section>
+		<!-- Way To Use Section End -->
 
-					<div class="col-sm-6">
-						<div class="job-card">
-							<div class="row align-items-center">
-								<div class="col-lg-3">
-									<div class="thumb-img">
-										<a href="job-details.html">
-											<img src="assets/img/company-logo/4.png" alt="company logo">
-										</a>
-									</div>
-								</div>
+		<!-- Companies Section Start -->
+		<section class="company-section pt-100 pb-70">
+			<div class="container">
+				<div class="section-title text-center">
+					<h2>Nuestras sucursales</h2>
+					<p>
+						Nuestra más alta prioridad es el cliente para quien tenemos nuestra mejor actitud de servicio.
+					</p>
+				</div>
 
-								<div class="col-lg-6">
-									<div class="job-info">
-										<h3>
-											<a href="job-details.html">Web Developer</a>
-										</h3>
-										<ul>
-											<li>Via <a href="#">MegaNews</a></li>
-											<li>
-												<i class='bx bx-location-plus'></i>
-												San Francisco, California
-											</li>
-											<li>
-												<i class='bx bx-filter-alt' ></i>
-												Development
-											</li>
-											<li>
-												<i class='bx bx-briefcase' ></i>
-												Freelance
-											</li>
-										</ul>
-									</div>
-								</div>
-
-								<div class="col-lg-3">
-									<div class="job-save">
-										<a href="#">
-											<i class='bx bx-heart'></i>
-										</a>
-										<p>
-											<i class='bx bx-stopwatch' ></i>
-											5 Hr Ago
-										</p>
-									</div>
-								</div>
+				<div class="row">
+					<div class="col-lg-3 col-sm-6" v-for="item in sedes">
+						<div class="company-card">
+							<div class="company-logo">
+								<a href="/">
+									<img src="@/assets/img/logos/logo-dark.png" alt="company logo">
+								</a>
 							</div>
-						</div>
-					</div>
-
-					<div class="col-sm-6">
-						<div class="job-card">
-							<div class="row align-items-center">
-								<div class="col-lg-3">
-									<div class="thumb-img">
-										<a href="job-details.html">
-											<img src="assets/img/company-logo/5.png" alt="company logo">
-										</a>
-									</div>
-								</div>
-
-								<div class="col-lg-6">
-									<div class="job-info">
-										<h3>
-											<a href="job-details.html">Digital Marketor</a>
-										</h3>
-										<ul>
-											<li>Via <a href="#">AB Marketer LTD</a></li>
-											<li>
-												<i class='bx bx-location-plus'></i>
-												Wellesley Rd, London
-											</li>
-											<li>
-												<i class='bx bx-filter-alt' ></i>
-												Marketing
-											</li>
-											<li>
-												<i class='bx bx-briefcase' ></i>
-												Freelance
-											</li>
-										</ul>
-									</div>
-								</div>
-
-								<div class="col-lg-3">
-									<div class="job-save">
-										<a href="#">
-											<i class='bx bx-heart'></i>
-										</a>
-										<p>
-											<i class='bx bx-stopwatch' ></i>
-											6 Hr Ago
-										</p>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-
-					<div class="col-sm-6">
-						<div class="job-card">
-							<div class="row align-items-center">
-								<div class="col-lg-3">
-									<div class="thumb-img">
-										<a href="job-details.html">
-											<img src="assets/img/company-logo/6.png" alt="company logo">
-										</a>
-									</div>
-								</div>
-
-								<div class="col-lg-6">
-									<div class="job-info">
-										<h3>
-											<a href="job-details.html">UI/UX Designer</a>
-										</h3>
-										<ul>
-											<li>Via <a href="#">Design Hunter</a></li>
-											<li>
-												<i class='bx bx-location-plus'></i>
-												Zoo Rd, London
-											</li>
-											<li>
-												<i class='bx bx-filter-alt' ></i>
-												Accountancy
-											</li>
-											<li>
-												<i class='bx bx-briefcase' ></i>
-												Freelance
-											</li>
-										</ul>
-									</div>
-								</div>
-
-								<div class="col-lg-3">
-									<div class="job-save">
-										<a href="#">
-											<i class='bx bx-heart'></i>
-										</a>
-										<p>
-											<i class='bx bx-stopwatch' ></i>
-											8 Hr Ago
-										</p>
-									</div>
-								</div>
+							<div class="company-text">
+								<h3>{{item.name}}</h3>
+								<p>
+									<i class='bx bx-location-plus'></i>
+									{{item.address}}
+								</p>
+								<a href="/" class="company-btn">
+									{{item.cant}} Vacantes
+								</a>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</section> -->
+		</section>
+		<!-- Companies Section End -->
     </div>
 </template>
 <script>
 import Banner from '@/components/home/Banner.vue';
+import sedes from '@/data/sedes';
 export default {
     components: {
 		Banner,
+	},
+	data(){
+		return{
+			sedes,
+		}
 	}
 }
 </script>
